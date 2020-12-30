22eff287ea4ce2aec656af09840fbf3f5445bced
DispatchCommit <dispatchcommit@gmail.com>
2020-12-29 16:42:02 -0800

    Fix clients tracking
    Fix issue where client ids were not properly being tracked due never being initially set upon joining a lobby.

--- src/index.ts
+++ src/index.ts
@@ -46,7 +46,7 @@ app.set('view engine', 'pug')
 app.use(morgan('combined'))
 
 let connectionCount = 0;
-let address = process.env.ADDRESS;
+let address = process.env.ADDRESS || 'vc.dispatchplays.tv';
 if (!address) {
 	logger.error('You must set the ADDRESS environment variable.');
 	process.exit(1);
@@ -63,13 +63,41 @@ app.get('/health', (req, res) => {
 		address,
 		name: process.env.NAME
 	});
-})
+});
 
+// API
+app.get('/v1/lobby/:lobby', (req, res) => {
+  const lobbyParam = req.params.lobby;
+  if ( lobbyParam && typeof lobbyParam === 'string' && lobbyParam.length === 6 ) {
+    let allClients: any = {};
+    let socketsInLobby = Object.keys(io.sockets.adapter.rooms[lobbyParam].sockets);
+    for (let s of socketsInLobby) {
+      if ( clients.has(s) ) {
+        allClients[s] = clients.get(s);
+      }
+    }
+
+    res.json({
+      lobby: lobbyParam,
+      playerCount: socketsInLobby.length,
+      clients: allClients,
+    });
+  }
+});
+
+//-------------------------
+// Middleware Version Check
 io.use((socket, next) => {
 	const userAgent = socket.request.headers['user-agent'];
-	const matches = /^CrewLink\/(\d+\.\d+\.\d+) \((\w+)\)$/.exec(userAgent);
+	const matches = /^(?:CrewLink|DispatchLink)\/(\d+\.\d+\.\d+) \((\w+)\)$/.exec(userAgent);
 	const error = new Error() as any;
-	error.data = { message: 'The voice server does not support your version of CrewLink.\nSupported versions: ' + Array.from(supportedCrewLinkVersions).join() };
+	error.data = { message: 'The voice server does not support your version of DispatchLink.\nSupported versions: ' + Array.from(supportedCrewLinkVersions).join() };
+
+	console.log( `Client connected running v${matches[1]}` );
+
+	// disable client version check
+	return next();
+
 	if (!matches) {
 		next(error);
 	} else {
@@ -83,23 +111,34 @@ io.use((socket, next) => {
 	}
 });
 
+
 io.on('connection', (socket: socketIO.Socket) => {
 	connectionCount++;
 	logger.info("Total connected: %d", connectionCount);
 	let code: string | null = null;
 
-	socket.on('join', (c: string, id: number, clientId: number) => {
-		if (typeof c !== 'string' || typeof id !== 'number' || typeof clientId !== 'number') {
+
+	//-------------
+	// Player Joins
+	socket.on('join', (lobbyCode: string, playerId: number, clientId: number) => {
+		if (
+		  typeof lobbyCode !== 'string' ||
+      typeof playerId !== 'number' ||
+      typeof clientId !== 'number'
+    ) {
 			socket.disconnect();
-			logger.error(`Socket %s sent invalid join command: %s %d %d`, socket.id, c, id, clientId);
+			logger.error(`Socket %s sent invalid join command: %s %d %d`, socket.id, lobbyCode, playerId, clientId);
 			return;
 		}
 
 		let otherClients: any = {};
-		if (io.sockets.adapter.rooms[c]) {
-			let socketsInLobby = Object.keys(io.sockets.adapter.rooms[c].sockets);
+		if (io.sockets.adapter.rooms[lobbyCode]) {
+			let socketsInLobby = Object.keys(io.sockets.adapter.rooms[lobbyCode].sockets);
 			for (let s of socketsInLobby) {
-				if (clients.has(s) && clients.get(s).clientId === clientId) {
+				if (
+				  clients.has(s) &&
+          clients.get(s).clientId === clientId
+        ) {
 					socket.disconnect();
 					logger.error(`Socket %s sent invalid join command, attempted spoofing another client`);
 					return;
@@ -108,45 +147,90 @@ io.on('connection', (socket: socketIO.Socket) => {
 					otherClients[s] = clients.get(s);
 			}
 		}
-		code = c;
+
+		code = lobbyCode;
+
 		socket.join(code);
-		socket.to(code).broadcast.emit('join', socket.id, {
-			playerId: id,
-			clientId: clientId === Math.pow(2, 32) - 1 ? null : clientId
-		});
+
+		const client = {
+      playerId: playerId,
+      clientId: clientId === Math.pow(2, 32) - 1 ? null : clientId
+    };
+
+		// Alert other clients of new user
+    clients.set(socket.id, client);
+		socket.to(code).broadcast.emit('join', socket.id, client);
+
+		// Send client list to joining user
 		socket.emit('setClients', otherClients);
+
+		logger.info( `[JOIN] lobbyCode: ${lobbyCode} playerId: ${playerId} clientId: ${clientId}` );
 	});
 
+
+	//------------------
+	// Player Id Changed
 	socket.on('id', (id: number, clientId: number) => {
-		if (typeof id !== 'number' || typeof clientId !== 'number') {
+		if (
+		  typeof id !== 'number' ||
+      typeof clientId !== 'number'
+    ) {
 			socket.disconnect();
 			logger.error(`Socket %s sent invalid id command: %d %d`, socket.id, id, clientId);
 			return;
 		}
+
 		let client = clients.get(socket.id);
-		if (client != null && client.clientId != null && client.clientId !== clientId) {
+		if (
+		  client != null &&
+      client.clientId != null &&
+      client.clientId !== clientId
+    ) {
 			socket.disconnect();
 			logger.error(`Socket %s sent invalid id command, attempted spoofing another client`);
 			return;
 		}
+
 		client = {
 			playerId: id,
 			clientId: clientId === Math.pow(2, 32) - 1 ? null : clientId
 		};
 		clients.set(socket.id, client);
 		socket.to(code).broadcast.emit('setClient', socket.id, client);
-	})
+
+		logger.info( `[ID] Lobby: ${code} Client:`, client );
+	});
 
 
+	//------------
+  // Player Left
 	socket.on('leave', () => {
 		if (code) {
-			socket.leave(code);
-			clients.delete(socket.id);
-		}
-	})
+		  logger.info( `[LEAVE] Socket.id:`, socket.id );
 
+		  if ( clients.has(socket.id) ) {
+        let client = clients.get(socket.id);
+        logger.info( `[LEAVE] Client:`, client );
+        logger.info( `[LEAVE] lobbyCode: ${code} playerId: ${client?.playerId} clientId: ${client?.clientId}` );
+      }
+
+			socket.leave(code);
+      clients.delete(socket.id);
+
+      code = null;
+		}
+	});
+
+
+	//-------------
+  // Signal Relay
 	socket.on('signal', (signal: Signal) => {
-		if (typeof signal !== 'object' || !signal.data || !signal.to || typeof signal.to !== 'string') {
+		if (
+		  typeof signal !== 'object' ||
+      !signal.data ||
+      !signal.to ||
+      typeof signal.to !== 'string'
+    ) {
 			socket.disconnect();
 			logger.error(`Socket %s sent invalid signal command: %j`, socket.id, signal);
 			return;
@@ -158,15 +242,20 @@ io.on('connection', (socket: socketIO.Socket) => {
 		});
 	});
 
+
+	//--------------------
+	// Client Disconnected
 	socket.on('disconnect', () => {
 		clients.delete(socket.id);
 		connectionCount--;
 		logger.info("Total connected: %d", connectionCount);
-	})
+	});
 
 })
 
+// Start server
 server.listen(port);
+
 (async () => {
-	logger.info('CrewLink Server started: %s', address);
+	logger.info('DispatchLink Server started: %s', address);
 })();
